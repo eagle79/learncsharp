481c521a6e5911c7e9e76859593a66802a0a5eaf
Aaron Craven <cravenaaron@hotmail.com>
2020-03-03 21:24:43 -0500

    Add first C# project with Hello World console app.

---
+++ FirstCSharp/FirstCSharpSolution/FirstCSharpProject/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstCSharpProject
+{
+    class HelloWorld
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
